fc364d952eb6db21676a2079ac578e55f529eb35
Jeromy Condon <Jeromy@rufusmedia.com>
2012-04-11 15:48:03 -0700

    Added mobile navigation select menu code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// INTENTIONALLY LEFT BLANK ;)
+jQuery(document).ready(function(){  
+  // MOBILE NAVIGATION 
+  jQuery(".mobile-menu").change(function(){
+    if (jQuery(this).val()!='') {
+    window.location.href=jQuery(this).val();
+    }
+  });
+}); 
