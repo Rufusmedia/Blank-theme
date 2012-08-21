0be682a6b706c45c91e48e1fe335ed2f86a83ec9
jeromy <jeromy@rufusmedia.com>
2012-08-20 21:18:31 -0700

    Clean indentation and formatting

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+// use jQuery instead of "$" to avoid conflict
 jQuery(document).ready(function(){  
-  // MOBILE NAVIGATION 
-  jQuery(".mobile-menu").change(function(){
-    if (jQuery(this).val()!='') {
-    window.location.href=jQuery(this).val();
-    }
-  });
+	// MOBILE NAVIGATION 
+	jQuery(".mobile-menu").change(function(){
+	if (jQuery(this).val()!='') {
+		window.location.href=jQuery(this).val();
+		}
+	});
 }); 
